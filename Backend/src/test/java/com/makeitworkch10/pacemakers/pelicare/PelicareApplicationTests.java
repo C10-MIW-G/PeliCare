b577e5c116c3e882a5fc3b7b45943718512a3e11
Ruben de Vries <devriesruben@outlook.com>
2023-01-26 13:16:51 +0100

    PC-3 Initial commit backend

---
+++ Backend/src/test/java/com/makeitworkch10/pacemakers/pelicare/PelicareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makeitworkch10.pacemakers.pelicare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PelicareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
