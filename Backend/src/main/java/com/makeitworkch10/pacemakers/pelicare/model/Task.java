f9189eb0c64f2c4dd9ca0eb41bbf125096bf67f4
Paul <pccmoonen@gmail.com>
2023-01-30 15:06:27 +0100

    naar java 17

--- Backend/src/main/java/com/makeitworkch10/pacemakers/pelicare/model/Task.java
+++ Backend/src/main/java/com/makeitworkch10/pacemakers/pelicare/model/Task.java
@@ -1,12 +1,12 @@
 package com.makeitworkch10.pacemakers.pelicare.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
 
 /**
  * @author Paul Moonen
