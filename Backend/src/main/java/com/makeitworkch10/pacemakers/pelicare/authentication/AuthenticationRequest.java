345096b5ffdbabf0519962854b210af92558316e
Ruben de Vries <devriesruben@outlook.com>
2023-01-31 15:59:45 +0100

    Added security JWT authentication and authorisation to the backend

---
+++ Backend/src/main/java/com/makeitworkch10/pacemakers/pelicare/authentication/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.makeitworkch10.pacemakers.pelicare.authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String email;
+    private String password;
+}
---
+++ Backend/src/main/java/com/makeitworkch10/pacemakers/pelicare/user/Role.java
@@ -0,0 +1,12 @@
+package com.makeitworkch10.pacemakers.pelicare.user;
+
+/**
+ * @author Ruben de Vries
+ * Defining User Roles.
+ */
+
+public enum Role {
+    USER,
+    CIRCLE_ADMIN,
+    SITE_ADMIN
+}
