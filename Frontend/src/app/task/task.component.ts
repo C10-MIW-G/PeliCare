6886c93153378059d00fe617bb897b27f14845a6
Paul <pccmoonen@gmail.com>
2023-02-10 14:27:56 +0100

    PC-20 Tasks can be edited

--- Frontend/src/app/task/task.component.ts
+++ Frontend/src/app/task/task.component.ts
@@ -13,14 +13,16 @@ import { Task } from '../task';
 })
 export class TaskComponent implements OnInit{
 
-	public task: Task;
-	public id: Number;
+	public taskId: number;
 	public title: string;
 	public description: string;
 	public carecircleName: String;
-	public incompleteFields: boolean;
-
+	public incompleteFields: boolean; 	// to display an error message if necessary
+	private routeToThis: String = ""; 	// how we got here determines which api-route will be used next
+	public message: String = "";		// feedback to user: what does this form do (creation or update of Task)	
+	private newTask: boolean = false; 	// making or editing requires different data and api calls
   	public careCircleId: number;
+
   	constructor (
     	private route: ActivatedRoute,
     	private taskservice: TaskService,
@@ -29,44 +31,79 @@ export class TaskComponent implements OnInit{
   	) {}
 
   	ngOnInit(): void {
-		this.incompleteFields = false;
-    	this.careCircleId = Number(this.route.snapshot.paramMap.get('circleId'));
-    	this.findCircleName();    
+		this.incompleteFields = false;	
+		this.careCircleId = Number(this.route.snapshot.paramMap.get('circleId'));
+		this.routeToThis = this.route.toString();
+		
+		if(this.routeToThis.includes("create")){ this.prepareCreationOfTask(); }
+		if(this.routeToThis.includes("edit")){ this.prepareEditingOfTask(); }	
+
+    	this.displayCircleName();    
   	}
 
-	// to display the name of the Care Circle to which we well add this new Task
-  	private findCircleName() {
+	private prepareEditingOfTask() {
+		this.message = "Edit Task";
+		this.taskId = Number(this.route.snapshot.paramMap.get('taskId'));
+		this.taskservice.getTaskById(this.taskId)
+			.subscribe({
+				next: (response: Task) => {
+					this.title = response.title;
+					this.description = response.description;
+				},
+				error: (error: HttpErrorResponse) => { console.log(error.message); }
+			});
+	}
+
+	private prepareCreationOfTask() {
+		this.newTask = true;
+		this.message = "Create new Task";
+	}
+	
+  	private displayCircleName() {
     	this.carecircleService.getCareCircleById(this.careCircleId)
       	.subscribe({
         	next: (response: CareCircle) => {
-          	this.carecircleName = response.name;
+          		this.carecircleName = response.name;
         	},
-        	error: (error: HttpErrorResponse) => {
-          		console.log(error.message);
-        	}
+        	error: (error: HttpErrorResponse) => { console.log(error.message); }
       	});
   	}
 
-  	saveNewTask() {
-		
-    	if (this.title && this.description) {      
-
-      		this.taskservice.saveNewTaskData({
-        		title: this.title,
-        		description: this.description,
-        		careCircleId: this.careCircleId
-      		}) 
-      		.subscribe({
-        		complete: ()=> {
-          		console.log(Response.toString);
-          		this.router.navigateByUrl(`/carecircle/${this.careCircleId}`)},
-          		error: ()=> {alert( "something went wrong");
-        		}
-      		});
-      	} 
-		
-		else {
-			this.incompleteFields = true;
+	save() {
+		if(this.title && this.description) {
+			if (this.newTask) { this.saveNewTask()}
+			else { this.updateTask()}
 		}
+		else { this.incompleteFields = true; }
+	}
+
+	updateTask() {
+		this.taskservice.updateTask({
+			title: this.title,
+			description: this.description,
+			id: this.taskId
+		}).subscribe({
+			complete: ()=> {
+				console.log(Response.toString);
+				this.router.navigateByUrl(`/carecircle/${this.careCircleId}`)
+			},
+			error: ()=> {alert( "something went wrong"); }
+		});
+	}
+
+  	saveNewTask() {	      
+
+		this.taskservice.saveNewTaskData({
+			title: this.title,
+			description: this.description,
+			careCircleId: this.careCircleId
+		}) 
+		.subscribe({
+			complete: ()=> {
+				console.log(Response.toString);
+				this.router.navigateByUrl(`/carecircle/${this.careCircleId}`)
+			},
+			error: ()=> {alert( "something went wrong"); }
+		});    		
     }  
 }
