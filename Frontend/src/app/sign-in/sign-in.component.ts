8b417fd446ef62561f135cb5eb4255c2a3d95fa5
MaaikeFeenstra <118736436+MaaikeFeenstra@users.noreply.github.com>
2023-02-13 10:20:53 +0100

    Lay out aangepast

--- Frontend/src/app/sign-in/sign-in.component.ts
+++ Frontend/src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
@@ -8,8 +8,10 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css'],
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit{
   form: FormGroup;
+  public incompleteEmailField: boolean; 
+    public incompletePasswordField: boolean;
 
   constructor(
     private fb: FormBuilder,
@@ -21,9 +23,20 @@ export class SignInComponent {
       password: ['', Validators.required],
     });
   }
+  ngOnInit(): void {
+    this.incompleteEmailField = false;
+    this.incompletePasswordField = false;    
+  }  
 
   login() {
     const val = this.form.value;
+    if (!val.email){
+      this.incompleteEmailField = true;
+    }
+
+    if (!val.password){
+      this.incompletePasswordField = true;
+    }
 
     if (val.email && val.password) {
       this.authService.login(val.email, val.password).subscribe({
