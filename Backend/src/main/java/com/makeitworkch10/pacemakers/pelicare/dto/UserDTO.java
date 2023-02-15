8f0ea40d28187be067a4dd0ca803e014cc6ec3f7
Maaike de Jong <117643803+ScorpioKardia@users.noreply.github.com>
2023-02-15 15:17:19 +0100

    PC-19 added functionality to add user tocarecircle

---
+++ Backend/src/main/java/com/makeitworkch10/pacemakers/pelicare/dto/UserDTO.java
@@ -0,0 +1,7 @@
+package com.makeitworkch10.pacemakers.pelicare.dto;/**
+* @author Maaike de Jong
+* Dit is wat het programma doet
+*/
+
+public class UserDTO {
+}
---
+++ Backend/src/main/java/com/makeitworkch10/pacemakers/pelicare/service/mappers/UserDTOMapper.java
@@ -0,0 +1,7 @@
+package com.makeitworkch10.pacemakers.pelicare.service.mappers;/**
+* @author Maaike de Jong
+* Dit is wat het programma doet
+*/
+
+public class UserDTOMapper {
+}
